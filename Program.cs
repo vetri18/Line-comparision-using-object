cb12aa0f613c965fb453af487d94048e294e2ef7
vetrivel <vetrivel.kce@gmail.com>
2022-08-12 12:55:54 +0530

    welcome to line comparision and computation using object

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+        }
+    }
+}
