23360e98225264d57f1caeb50f8c8b6664bbc7f3
vetrivel <vetrivel.kce@gmail.com>
2022-08-12 13:06:20 +0530

    uc2

---
+++ EqualLength.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    public class EqualLength
+    {
+        public void LengthOfLine(double x1, double y1, double x2, double y2, double a1, double b1, double a2, double b2)
+        {
+            double lengthOfLine;
+            double lengthOfLine1;
+
+            lengthOfLine = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("Length of a given Line is: " + lengthOfLine);
+
+            lengthOfLine1 = Math.Sqrt(Math.Pow((a2 - a1), 2) + Math.Pow((b2 - b1), 2));
+            Console.WriteLine("Length of a given Line is: " + lengthOfLine1);
+
+            Console.WriteLine(lengthOfLine.Equals(lengthOfLine1));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         public static void Main(string[] args)
         {
             //Welcome message
-            Console.WriteLine("Welcome to line computation program");
+            Console.WriteLine("Welcome to line computation program.");
 
             //Taking line points from user
             Console.WriteLine("Enter the line co-ordinates as x1,y1,x2,y2: ");
@@ -14,11 +14,15 @@
             double x2 = Convert.ToDouble(Console.ReadLine());
             double y2 = Convert.ToDouble(Console.ReadLine());
 
-
+            Console.WriteLine("Enter the line co-ordinates as a1,b1,a2,b2: ");
+            double a1 = Convert.ToDouble(Console.ReadLine());
+            double b1 = Convert.ToDouble(Console.ReadLine());
+            double a2 = Convert.ToDouble(Console.ReadLine());
+            double b2 = Convert.ToDouble(Console.ReadLine());
 
             //Calling Length of line function
-            CalculateLength lineComparison = new CalculateLength();
-            lineComparison.LengthOfLine(x1, y1, x2, y2);
+            EqualLength equalLength = new EqualLength();
+            equalLength.LengthOfLine(x1, y1, x2, y2, a1, b1, a2, b2);
         }
     }
 }
