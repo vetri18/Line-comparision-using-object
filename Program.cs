f71d69a9c5afba2ed2db230401eed16938d68276
vetrivel <vetrivel.kce@gmail.com>
2022-08-12 12:59:51 +0530

    uc1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿namespace EmployeeWage
+﻿namespace LineComparison
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            //Welcome message
+            Console.WriteLine("Welcome to line computation program");
+
+            //Taking line points from user
+            Console.WriteLine("Enter the line co-ordinates as x1,y1,x2,y2: ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+
+
+            //Calling Length of line function
+            CalculateLength lineComparison = new CalculateLength();
+            lineComparison.LengthOfLine(x1, y1, x2, y2);
         }
     }
 }
